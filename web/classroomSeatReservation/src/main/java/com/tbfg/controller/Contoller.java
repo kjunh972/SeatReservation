49663c3e6574536b12fdba9a1ec47474eacbc238
kjunh972 <kjunh972@gmail.com>
2024-07-31 23:21:56 +0900

    강의실 좌석 예약

--- web/classroomSeatReservation/src/main/java/com/tbfg/controller/Contoller.java
+++ web/classroomSeatReservation/src/main/java/com/tbfg/controller/Contoller.java
@@ -2,6 +2,8 @@ package com.tbfg.controller;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Random;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
@@ -14,6 +16,9 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.tbfg.dao.ClassroomDAO;
 import com.tbfg.dao.UserDAO;
 import com.tbfg.dto.ClassroomDTO;
@@ -21,21 +26,23 @@ import com.tbfg.dto.TimeTableDTO;
 
 @Controller
 public class Contoller {
-
+	private ClassroomDTO classroomDTO = new ClassroomDTO();
+    private TimeTableDTO timetableDTO = new TimeTableDTO();
+    
     @Autowired
     private JdbcTemplate jdbcTemplate; 
 
-    // 홈페이지로 이동하는 메서드
+    // 메인페이지 메서드
     @GetMapping("/index")
     public String index() {
         return "index";
     }
     
-    // 강의실 즐겨찾기 화면으로 이동하는 메서드
+    // 강의실 즐겨찾기 메서드
     @GetMapping("/classroomLike")
     public String classroomLike(Model model) {
-        ClassroomDAO classroomDAO = new ClassroomDAO(jdbcTemplate);
-        
+    	ClassroomDAO classroomDAO = new ClassroomDAO(jdbcTemplate);
+    	
         // 사용자가 즐겨찾기한 강의실 버튼 목록 가져오기
         classroomDAO.getTimetableClassrooms("kjunh972");
         List<String> favoriteClassrooms = classroomDAO.getFavoriteClassrooms("kjunh972");
@@ -46,33 +53,73 @@ public class Contoller {
         return "classroomLike";
     }
 
-    // 선택한 강의실의 상태를 조회하는 메서드
+    // 강의실 좌석 예약 메서드 
     @PostMapping("/classroomStatus") 
-    public String classroomStatus(String classroomName, Model model) { 
-        ClassroomDTO classroom = new ClassroomDTO(); 
-        ClassroomDAO classroomDAO = new ClassroomDAO(jdbcTemplate); 
-        classroom.setClassroomName(classroomName); 
-        classroom.setSeatCount(48); 
+    public String classroomStatus(String classroomName,
+                                  @RequestParam("selectHours") String selectHoursJson,
+                                  Model model) throws JsonProcessingException {
 
-        // 선택한 강의실에 대한 예약된 좌석 조회
-        List<Integer> reservedSeats = classroomDAO.getReservedSeats(classroomName, "kjunh972");
+        // JDBC 템플릿을 사용하여 ClassroomDAO 객체를 생성
+        ClassroomDAO classroomDAO = new ClassroomDAO(jdbcTemplate);
+        // classroomDTO 객체에 사용자가 입력한 강의실 이름을 설정
+        classroomDTO.setClassroomName(classroomName);     
+        // 강의실의 좌석 수를 48로 설정
+        classroomDTO.setSeatCount(48);
 
-        // 예약된 좌석 상태 설정
-        for (Integer seat : reservedSeats) { 
-            classroom.reserveSeat(seat); 
+        // ObjectMapper를 사용하여 JSON 문자열을 Java의 List<Integer>로 변환
+        ObjectMapper objectMapper = new ObjectMapper();
+        List<Integer> selectHours = objectMapper.readValue(selectHoursJson, new TypeReference<List<Integer>>() {});
+        // 변환된 시간대를 classroomDTO 객체에 설정
+        classroomDTO.setSelectHours(selectHours);
+
+        // DAO를 통해 선택한 시간대에 이미 예약된 좌석들을 조회
+        List<Integer> reservedSeats = classroomDAO.getReservedSeats(classroomName, selectHours);
+        // 예약된 좌석을 classroomDTO에 반영하여 예약 상태를 업데이트
+        for (Integer seat : reservedSeats) {
+            classroomDTO.reserveSeat(seat);
         }
 
-        // 모델에 ClassroomDTO 객체 추가
-        model.addAttribute("classroom", classroom); 
-
+        model.addAttribute("classroom", classroomDTO);
+        
         return "classroomStatus";
     }
-    
+
+    // 강의실 좌석 예약 메서드
+    @PostMapping("/reserveSeat")  
+    public String reserveSeat(String classroomName, Integer seatNumber,
+                              Model model) throws JsonProcessingException {
+
+        // JDBC 템플릿을 사용하여 ClassroomDAO 객체를 생성
+        ClassroomDAO classroomDAO = new ClassroomDAO(jdbcTemplate);
+
+        // 0부터 999 사이의 랜덤 번호를 생성
+        int randomNum = new Random().nextInt(1000);
+        // classroomDTO에 랜덤 번호 저장
+        classroomDTO.setRandomNum(randomNum);
+        
+        // DAO를 사용하여 좌석을 예약하고, 해당 좌석에 랜덤 번호를 설정
+        classroomDAO.reserveSeat("kjunh972", classroomName, seatNumber, randomNum);
+        // 사용자가 선택한 시간대에 대해 예약된 시간 정보를 ReservationHour 테이블에 추가
+        for (Integer hour : classroomDTO.getSelectHours()) {
+            classroomDAO.addReservationHour(randomNum, hour);
+        }
+        
+        String hours = classroomDTO.getSelectHours().stream().map(String::valueOf).collect(Collectors.joining("시, "));
+
+        model.addAttribute("seatNumber", seatNumber);
+        model.addAttribute("selectHours", hours);
+        model.addAttribute("randomNum", randomNum);
+        model.addAttribute("classroom", classroomDTO);
+        
+        return "reserveSeat";
+    }
+
+
     // 선택한 강의 번호에 해당하는 강의실 목록을 조회하는 메서드
     @PostMapping("/classroomSelect")
     public String classroomSelect(String classNum, Model model) {
-        ClassroomDAO classroomDAO = new ClassroomDAO(jdbcTemplate);
-        
+    	ClassroomDAO classroomDAO = new ClassroomDAO(jdbcTemplate);
+    	
         // 선택한 강의 번호에 해당하는 강의실 목록 조회
         List<String> classrooms = classroomDAO.getClassrooms(classNum);
         
@@ -86,8 +133,8 @@ public class Contoller {
     // 모든 강의 번호 목록을 조회하는 메서드
     @PostMapping("/classSelect")
     public String getClassSelect(String classNumber, Model model) {
-        ClassroomDAO classroomDAO = new ClassroomDAO(jdbcTemplate);
-        
+    	ClassroomDAO classroomDAO = new ClassroomDAO(jdbcTemplate);
+    	
         // 모든 강의 번호 목록 조회
         List<String> classNum = classroomDAO.getClassNum();
         
@@ -97,11 +144,22 @@ public class Contoller {
         return "classSelect";
     }
     
+    // 시간 선택하는 페이지를 보여주는 메서드
+    @PostMapping("/timeSelect")
+    public String timeSelect(String classroomName, Model model) {
+    	List<Integer> hours = Arrays.asList(9, 10, 11, 12, 1, 2, 3, 4, 5, 6, 7, 8); // 9시부터 8시까지의 시간
+    	classroomDTO.setClassroomName(classroomName); // 강의실 번호 설정
+    	
+    	// 모델에 ClassroomDTO 객체 추가
+        model.addAttribute("classroom", classroomDTO); 
+        model.addAttribute("hours", hours);
+        return "timeSelect";
+    }
+    
     @GetMapping("/timetable")
     public String timetable(Model model, @ModelAttribute("warning") String warning) {
-        
-        UserDAO userDAO = new UserDAO(jdbcTemplate);
-        String userId = "kjunh972"; // 사용자 ID
+    	UserDAO userDAO = new UserDAO(jdbcTemplate);
+    	String userId = "kjunh972"; // 사용자 ID
 
         // 사용자의 모든 시간표를 가져옴.
         List<TimeTableDTO> userTimeTable = userDAO.getTimetable(userId);
@@ -123,10 +181,7 @@ public class Contoller {
     @PostMapping("/timetable")
     public String postTimetable(String subject, String classroomName, String day, Integer startHour,
             Integer endHour, Model model) {
-
-        // `UserDAO`와 `TimeTableDTO`의 인스턴스를 생성합니다.
         UserDAO userDAO = new UserDAO(jdbcTemplate);
-        TimeTableDTO timetableDTO = new TimeTableDTO();
         String userId = "kjunh972"; // 사용자 ID
         
         try {
@@ -181,7 +236,7 @@ public class Contoller {
     // 시간표를 삭제하는 메서드
     @PostMapping("/deleteTimetable")
     public String deleteTimeTable(String delete_subject, Model model) {
-        UserDAO userDAO = new UserDAO(jdbcTemplate);
+    	UserDAO userDAO = new UserDAO(jdbcTemplate);
         String userId = "kjunh972"; // 사용자 ID
 
         // 시간표 삭제
--- web/classroomSeatReservation/src/main/java/com/tbfg/dao/ClassroomDAO.java
+++ web/classroomSeatReservation/src/main/java/com/tbfg/dao/ClassroomDAO.java
@@ -1,6 +1,7 @@
 package com.tbfg.dao;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -14,15 +15,50 @@ public class ClassroomDAO {
     // 생성자
     public ClassroomDAO(JdbcTemplate jdbcTemplate) { 
         this.jdbcTemplate = jdbcTemplate; // JdbcTemplate 객체 초기화
-    } 
-
-    // 강의실 이름과 사용자 아이디를 받아 예약된 좌석을 가져오는 메서드
-    public List<Integer> getReservedSeats(String classroomName, String userId) { 
-        // MySQL에서 예약된 좌석을 가져오기 위한 쿼리문
-        String sql = "SELECT reservSeat FROM Reservation WHERE classroom_name = ? AND user_id = ?";
-        // jdbcTemplate을 사용하여 SQL 쿼리를 실행하고 결과를 리스트로 반환
-        return jdbcTemplate.queryForList(sql, Integer.class, classroomName, userId);
     }
+    
+    // 좌석 예약을 추가하는 메서드
+    public int reserveSeat(String userId, String classroomName, Integer seatNumber, Integer randomNumber) {
+        // Reservation 테이블에 예약 정보 (사용자 ID, 강의실 이름, 좌석 번호, 랜덤 번호)를 삽입하는 SQL 쿼리
+        String sql = "INSERT INTO Reservation (user_id, classroom_name, reservSeat, reservNum) VALUES (?, ?, ?, ?)";
+        
+        // jdbcTemplate을 사용해 SQL 쿼리를 실행하고, 영향받은 행의 수를 반환
+        return jdbcTemplate.update(sql, userId, classroomName, seatNumber, randomNumber);
+    }
+
+    // 예약 번호와 시간대를 ReservationHour 테이블에 추가하는 메서드
+    public void addReservationHour(int reservNum, int hour) {
+        // ReservationHour 테이블에 예약 번호와 해당 시간대를 삽입하는 SQL 쿼리
+        String sql = "INSERT INTO ReservationHour (reservNum, reservHour) VALUES (?, ?)";
+        
+        // jdbcTemplate을 사용해 SQL 쿼리를 실행
+        jdbcTemplate.update(sql, reservNum, hour);
+    }
+
+    // 특정 강의실과 시간대에 예약된 좌석을 가져오는 메서드
+    public List<Integer> getReservedSeats(String classroomName, List<Integer> hours) {
+        // 만약 hours 리스트가 null이거나 비어있다면, 빈 리스트를 반환 (쿼리를 실행하지 않음)
+        if (hours == null || hours.isEmpty()) {
+            System.out.println("빈 리스트 반환");
+            return List.of(); // 빈 리스트 반환
+        }
+        
+        // 예약된 좌석을 가져오는 SQL 쿼리 (Reservation과 ReservationHour 테이블을 조인)
+        String sql = "SELECT r.reservSeat "
+                   + "FROM Reservation r "
+                   + "JOIN ReservationHour rh ON r.reservNum = rh.reservNum "
+                   + "WHERE r.classroom_name = ?"  // 강의실 이름에 대해 필터링
+                   + "AND rh.reservHour IN (?)";  // 시간대 필터링 (in 절 사용)
+        
+        // List<Integer> (hours 리스트)를 쿼리에 사용하기 위해 쉼표로 구분된 문자열로 변환
+        String hoursParam = hours.stream()
+                                 .map(String::valueOf)  // 각 정수를 문자열로 변환
+                                 .collect(Collectors.joining(",")); // 쉼표로 연결하여 단일 문자열로 만듦
+        
+        // 변환된 hoursParam을 사용하여 SQL 쿼리를 실행하고, 결과를 List<Integer>로 반환
+        return jdbcTemplate.queryForList(sql, Integer.class, classroomName, hoursParam);
+    }
+
     
     // 사용자 아이디를 받아 즐겨찾기한 강의실 목록을 가져오는 메서드
     public List<String> getFavoriteClassrooms(String userId) {
@@ -36,7 +72,7 @@ public class ClassroomDAO {
     public void getTimetableClassrooms(String userId) {
         // MySQL에서 즐겨찾기할 강의실 목록을 가져와 favoriteClassrooms에 추가하는 쿼리문
         String sql = "INSERT INTO favoriteClassrooms (user_id, classroom_num) "
-                   + "SELECT DISTINCT ?, classroomName AS classroom_num "
+                   + "SELECT DISTINCT ?, classroomName "
                    + "FROM stutimetable WHERE user_id = ? AND (user_id, classroomName) "
                    + "NOT IN (SELECT user_id, classroom_num FROM favoriteClassrooms)";
 
